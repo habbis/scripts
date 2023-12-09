945453d700a8d962f751a055316bdc3b14bfd96f
Eirik Habbestad Helleve <eirik-habbestad.helleve@telenor.no>
2023-12-09 16:49:20 +0100

    changes

---
+++ mysql/example_db.sql
@@ -0,0 +1,75 @@
+
+--
+-- Table structure for table `host_vars`
+--
+
+DROP TABLE IF EXISTS `host_vars`;
+CREATE TABLE `host_vars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_hosts` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_hosts` (`id_hosts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `hostgroup`
+--
+
+DROP TABLE IF EXISTS `hostgroup`;
+CREATE TABLE `hostgroup` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_index` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `server`
+--
+
+DROP TABLE IF EXISTS `hosts`;
+CREATE TABLE `hosts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `FQDN` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `env` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `state` enum('new','present','absent','waiting') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `FQDN_index` (`FQDN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `interface`
+--
+
+DROP TABLE IF EXISTS `interface`;
+CREATE TABLE `interface` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_hosts` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_vlan` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_vlan` (`id_vlan`),
+  KEY `id_server` (`id_hosts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `vlan`
+--
+
+DROP TABLE IF EXISTS `vlan`;
+CREATE TABLE `vlan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `vlanid` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `vlan` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cidr` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `site` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `state` enum('new','present','absent') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cidr_index` (`cidr`),
+  UNIQUE KEY `vlanid_index` (`vlanid`),
+  KEY `vlan_index` (`vlan`)
+) ENGINE=InnoDB AUTO_INCREMENT=427 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
